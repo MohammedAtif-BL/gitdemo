c31617037e79e61a062d32c1ea4aec11bc00b8a6
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-07-12 16:04:41 +0530

    added: Sub of 2 numbers

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         int a  = 4;
         int b = 5;
-        int sum = a + b;
-        System.out.println(sum);
+        int sub = a - b;
+        System.out.println(sub);
     }
 }
