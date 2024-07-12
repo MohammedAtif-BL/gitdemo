09b2b77089983c2769b73b68c83e97ab03634c1f
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2024-07-12 16:01:52 +0530

    Added : 2 numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int a  = 4;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
